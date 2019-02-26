ddb083d0c77eb0d007e2dfb029246f068262e529
ffabinhoo <ffabinhoo@gmail.com>
2019-02-26 22:43:07 +1100

    modal list

--- src/components/research.list.component.js
+++ src/components/research.list.component.js
@@ -1,66 +1,12 @@
 import {Component} from 'react';
 import * as React from 'react';
-import {Link} from 'react-router-dom';
-import axios from 'axios';
+import Research from './research.component';
 
-
-const Research = props => (
-    <tr>
-        <td>{props.research.name}</td>
-        <td>{props.research.description}</td>
-        <td>
-        {formatDate(props.research.data)}
-        </td>
-        <td  className={props.research.isEnabled ? 'isEnabled':'isDisabled'}>{JSON.stringify(props.research.isEnabled)}</td>
-        <td>
-            <Link to={"/edit/"+props.research._id}>Edit</Link>
-            
-            &nbsp;&nbsp;&nbsp;&nbsp;
-            <button type="button" >
-                delete
-            </button>
-        </td>
-    </tr>
-)
-
-
-
-function formatDate(date) {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
-    return [year, month, day].join('-');
-}
 
 export default class ResearchList extends Component {
     constructor(props) {
         super(props);
-        this.state = {researchers: []}
-        //this.goToEdit = this.goToEdit.bind(this);
-    }
-
-    componentDidMount() {
-        axios.get('http://localhost:4000/researchers/')
-        .then(response => {
-                this.setState({researchers: response.data});
-        }) 
-        .catch(function(error){
-            console.log(error);
-        });
-    }
-
-    showModal = () => {
-        this.setState({ show: true });
-    };
-    
-
-    researcherList(){
-        return this.state.researchers.map(function(currentResearch, i){
-            return <Research research={currentResearch} key={i} />;
-        });
+        this.state = {researchers: []};
     }
 
     render() {
@@ -78,7 +24,7 @@ export default class ResearchList extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {this.researcherList()}
+                        <Research></Research>
                     </tbody>
                 </table>
             </div>
